5a78b48dd64e954bf743873168d751d33b087260
0xAhmd <ahmedhesham200987@gmail.com>
2025-06-07 21:26:07 +0300

    project setup init

---
+++ lib/features/auth/domain/entities/app_user.dart
@@ -0,0 +1,19 @@
+class AppUser {
+  // what user should have ?
+  // 1. uid
+  // 2. username
+  // 3. name
+  // 4. email
+
+  final String uid;
+  final String username;
+  final String name;
+  final String email;
+
+  AppUser({
+    required this.uid,
+    required this.username,
+    required this.name,
+    required this.email,
+  });
+}
